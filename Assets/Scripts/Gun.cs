eb57cad5090c62813a03a1d964b3b4f33d92ef7c
JBMedina19 <bene.medina019@gmail.com>
2023-04-18 15:03:16 +0800

    InitialCommit
    InitialCommit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform BulletSpawnpoint;
+    public GameObject BulletPrefab;
+    public float BulletSpeed;
+    public GameObject SpawnFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(SpawnFX, BulletSpawnpoint.position, BulletSpawnpoint.rotation);
+            var bullet = Instantiate(BulletPrefab, BulletSpawnpoint.position, BulletSpawnpoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = BulletSpawnpoint.forward * BulletSpeed;
+        }
+    }
+}
